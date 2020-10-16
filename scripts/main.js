d98d80b3d64bad41883d82b6ee711a562536acc9
Kirill <efremenkov1999@gmail.com>
2020-10-16 17:57:53 +0300

    add some sections

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
-    var mySwiper = new Swiper('.clothes-slider', {
-        loop: true,
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev'
-        },
-        slidesPerView: 4,
+    // drop down menu
+    // drop down menu
+    function initMenu() {
+        $(".sub-menu").hide();
+        $(".current_page_item .sub-menu").show();
+        $('#menu li a').click(
+
+            function () {
+                var checkElement = $(this).next();
+                if ((checkElement.is('ul')) && (checkElement.is(':visible'))) {
+                    return false;
+                }
+                if ((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
+                    console.log(checkElement.parentsUntil('#menu'));
+                    $('#menu ul:visible').not(checkElement.parentsUntil('#menu')).slideUp('normal');
+                    checkElement.slideDown('normal');
+                    return false;
+                }
+            });
+    }
+    $(function () {
+        initMenu();
     });
+
+    (function ($) {
+
+        $('.menu a').click(function (e) {
+            e.preventDefault();
+            $(this).parent('li').find('ul:first').slideToggle();
+        });
+
+    })(jQuery);
 });
