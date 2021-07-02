bfc0c8c08c725446b9233936156d186e8978d54b
Kirill <efremenkov1999@gmail.com>
2021-07-02 15:08:15 +0300

    refact cards

--- scripts/main.js
+++ scripts/main.js
@@ -29,4 +29,26 @@ document.addEventListener('DOMContentLoaded', () => {
         });
 
     })(jQuery);
+
+    const swiper = new Swiper('.clothes-slider', {
+        // Optional parameters
+        direction: 'vertical',
+        loop: true,
+
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        // And if we need scrollbar
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+    });
 });
